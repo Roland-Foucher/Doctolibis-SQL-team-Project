a67ee62d3957beab3eba47c69dee5918b962d43c
Roland Foucher <roland.foucher.pro@gmail.com>
2022-01-03 15:36:03 +0100

    init

---
+++ src/main/java/co/simplon/projetsql/App.java
@@ -0,0 +1,13 @@
+package co.simplon.projetsql;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
