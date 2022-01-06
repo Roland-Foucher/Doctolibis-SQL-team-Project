6f74491ce1ffd509ec0a754bcfd0c17a615f492c
khaledrazouk <khaled_razouk@hotmail.com>
2022-01-06 17:36:53 +0100

    add patientId/medecinId/apmtypeId to aptmt class

--- src/main/java/co/simplon/projetsql/entity/Appointment.java
+++ src/main/java/co/simplon/projetsql/entity/Appointment.java
@@ -6,16 +6,29 @@ public class Appointment {
 
     private int apm_id;
     private LocalDate date;
+    private int patient_id;
+    private int medecin_id;
+    private int apmType_id;
 
     // constructors
 
-    public Appointment(LocalDate date) {
-        this.date = date;
-    }
+   
 
-    public Appointment(int apm_id, LocalDate date) {
+   
+
+    public Appointment(int apm_id, LocalDate date, int patient_id, int medecin_id, int apmType_id) {
         this.apm_id = apm_id;
         this.date = date;
+        this.patient_id = patient_id;
+        this.medecin_id = medecin_id;
+        this.apmType_id = apmType_id;
+    }
+
+    public Appointment(LocalDate date, int patient_id, int medecin_id, int apmType_id) {
+        this.date = date;
+        this.patient_id = patient_id;
+        this.medecin_id = medecin_id;
+        this.apmType_id = apmType_id;
     }
 
     public Appointment() {
@@ -37,6 +50,30 @@ public class Appointment {
 
     public void setDate(LocalDate date) {
         this.date = date;
+    }
+
+    public int getPatient_id() {
+        return patient_id;
+    }
+
+    public void setPatient_id(int patient_id) {
+        this.patient_id = patient_id;
+    }
+
+    public int getMedecin_id() {
+        return medecin_id;
+    }
+
+    public void setMedecin_id(int medecin_id) {
+        this.medecin_id = medecin_id;
+    }
+
+    public int getApmType_id() {
+        return apmType_id;
+    }
+
+    public void setApmType_id(int apmType_id) {
+        this.apmType_id = apmType_id;
     }
 
 }
