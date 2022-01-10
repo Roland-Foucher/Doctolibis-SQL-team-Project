415dd1a962bd163c3ab8dd4affe6c2196c509bdd
Roland Foucher <roland.foucher.pro@gmail.com>
2022-01-10 17:14:43 +0100

    update database

---
+++ sql/Update_DOCTOLIBIS_2022-01-10_170628.sql
@@ -0,0 +1,301 @@
+-- MariaDB dump 10.19  Distrib 10.6.4-MariaDB, for osx10.16 (x86_64)
+--
+-- Host: 127.0.0.1    Database: DOCTOLIBIS
+-- ------------------------------------------------------
+-- Server version	10.6.4-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `apm_type`
+--
+
+DROP TABLE IF EXISTS `apm_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `apm_type` (
+  `apmType_id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(100) NOT NULL,
+  PRIMARY KEY (`apmType_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `apm_type`
+--
+
+/*!40000 ALTER TABLE `apm_type` DISABLE KEYS */;
+INSERT INTO `apm_type` VALUES (1,'PRESENTIEL'),(2,'VISIO');
+/*!40000 ALTER TABLE `apm_type` ENABLE KEYS */;
+
+--
+-- Table structure for table `appointment`
+--
+
+DROP TABLE IF EXISTS `appointment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `appointment` (
+  `apm_id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `medecin_id` int(11) NOT NULL,
+  `apmType_id` int(11) NOT NULL,
+  PRIMARY KEY (`apm_id`),
+  KEY `apmType_id` (`apmType_id`),
+  KEY `medecin_id` (`medecin_id`),
+  KEY `patient_id` (`patient_id`),
+  CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`apmType_id`) REFERENCES `apm_type` (`apmType_id`),
+  CONSTRAINT `appointment_ibfk_2` FOREIGN KEY (`medecin_id`) REFERENCES `medecin` (`medecin_id`),
+  CONSTRAINT `appointment_ibfk_3` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`patient_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `appointment`
+--
+
+/*!40000 ALTER TABLE `appointment` DISABLE KEYS */;
+INSERT INTO `appointment` VALUES (12,'2020-10-10',1,1,1);
+/*!40000 ALTER TABLE `appointment` ENABLE KEYS */;
+
+--
+-- Table structure for table `document`
+--
+
+DROP TABLE IF EXISTS `document`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `document` (
+  `doc_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  PRIMARY KEY (`doc_id`),
+  KEY `patient_id` (`patient_id`),
+  CONSTRAINT `document_ibfk_1` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`patient_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `document`
+--
+
+/*!40000 ALTER TABLE `document` DISABLE KEYS */;
+/*!40000 ALTER TABLE `document` ENABLE KEYS */;
+
+--
+-- Table structure for table `medecin`
+--
+
+DROP TABLE IF EXISTS `medecin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `medecin` (
+  `medecin_id` int(11) NOT NULL AUTO_INCREMENT,
+  `legalMention` varchar(50) NOT NULL,
+  `price` int(11) NOT NULL,
+  `adress` varchar(100) NOT NULL,
+  `presentation` varchar(255) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`medecin_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `medecin_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `medecin`
+--
+
+/*!40000 ALTER TABLE `medecin` DISABLE KEYS */;
+INSERT INTO `medecin` VALUES (1,'232',12,'blouop','medecin generaliste blablabla',2),(5,'chirurgien',10,'adresse','presentation',1);
+/*!40000 ALTER TABLE `medecin` ENABLE KEYS */;
+
+--
+-- Table structure for table `medecin_patient`
+--
+
+DROP TABLE IF EXISTS `medecin_patient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `medecin_patient` (
+  `patient_id` int(11) NOT NULL,
+  `medecin_id` int(11) NOT NULL,
+  KEY `patient_id` (`patient_id`),
+  KEY `medecin_id` (`medecin_id`),
+  CONSTRAINT `medecin_patient_ibfk_1` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`patient_id`),
+  CONSTRAINT `medecin_patient_ibfk_2` FOREIGN KEY (`medecin_id`) REFERENCES `medecin` (`medecin_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `medecin_patient`
+--
+
+/*!40000 ALTER TABLE `medecin_patient` DISABLE KEYS */;
+/*!40000 ALTER TABLE `medecin_patient` ENABLE KEYS */;
+
+--
+-- Table structure for table `medecin_speciality`
+--
+
+DROP TABLE IF EXISTS `medecin_speciality`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `medecin_speciality` (
+  `medecin_id` int(11) NOT NULL,
+  `spe_id` int(11) NOT NULL,
+  KEY `medecin_id` (`medecin_id`),
+  KEY `spe_id` (`spe_id`),
+  CONSTRAINT `medecin_speciality_ibfk_1` FOREIGN KEY (`medecin_id`) REFERENCES `medecin` (`medecin_id`),
+  CONSTRAINT `medecin_speciality_ibfk_2` FOREIGN KEY (`spe_id`) REFERENCES `speciality` (`spe_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `medecin_speciality`
+--
+
+/*!40000 ALTER TABLE `medecin_speciality` DISABLE KEYS */;
+/*!40000 ALTER TABLE `medecin_speciality` ENABLE KEYS */;
+
+--
+-- Table structure for table `patient`
+--
+
+DROP TABLE IF EXISTS `patient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `patient` (
+  `patient_id` int(11) NOT NULL AUTO_INCREMENT,
+  `secuNumber` varchar(50) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`patient_id`),
+  KEY `patient_ibfk_1` (`user_id`),
+  CONSTRAINT `patient_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `patient`
+--
+
+/*!40000 ALTER TABLE `patient` DISABLE KEYS */;
+INSERT INTO `patient` VALUES (1,'8908908263276',1),(2,'1111111111',2);
+/*!40000 ALTER TABLE `patient` ENABLE KEYS */;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `role` varchar(50) NOT NULL,
+  PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'ROLE_PATIENT'),(2,'ROLE_MEDECIN'),(3,'ROLE_ADMIN');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+
+--
+-- Table structure for table `speciality`
+--
+
+DROP TABLE IF EXISTS `speciality`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `speciality` (
+  `spe_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`spe_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `speciality`
+--
+
+/*!40000 ALTER TABLE `speciality` DISABLE KEYS */;
+INSERT INTO `speciality` VALUES (1,'generaliste'),(2,'dentiste'),(3,'chirurgien'),(4,'radiologue'),(5,'proctologue');
+/*!40000 ALTER TABLE `speciality` ENABLE KEYS */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phoneNumber` varchar(50) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Pierre','Dupont','pierredupont@gmail.com','1234',''),(2,'Jean','Paul','jeanpierre@gmail.com','1234',''),(3,'Jojo','Dujardin','jdujardin@gmail.com','1234',''),(4,'coco','jeanjean','coco.jeanea.fr','1234','');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+--
+-- Table structure for table `users_roles`
+--
+
+DROP TABLE IF EXISTS `users_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users_roles` (
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  KEY `role_id` (`role_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `users_roles_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`),
+  CONSTRAINT `users_roles_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users_roles`
+--
+
+/*!40000 ALTER TABLE `users_roles` DISABLE KEYS */;
+INSERT INTO `users_roles` VALUES (1,1),(2,2),(3,3),(4,1);
+/*!40000 ALTER TABLE `users_roles` ENABLE KEYS */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-01-10 17:07:02
